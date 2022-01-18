11aedf4fd9651ce4a7680bc98214729fb13a2261
Alan Pazetto <alanpazetto@Alans-MacBook-Air.local>
2022-01-18 18:44:42 -0300

    feat(install.sh): update file

--- install.sh
+++ install.sh
@@ -11,14 +11,23 @@ fi
 
 # Install brew formulas
 echo "\nInstalling Formulas"
-brew install bash-completion ruby python node nvm watchman yarn zsh zsh-syntax-highlighting
+brew install bash-completion ruby python node nvm watchman yarn zsh zsh-syntax-highlighting openjdk@8
 
 # Install brew cask formulas
 echo "\nInstalling CASK Formulas"
-brew cask install iterm2 osxfuse android-sdk vlc visual-studio-code postman react-native-debugger docker
+brew cask install iterm2 android-sdk android-platform-tools vlc visual-studio-code postman react-native-debugger docker slack brave-browser react-native-debugger
 
 #copying terminal configs
 echo "\nCopying Terminal Configs"
 cp .zshrc .bash_profile ~/
+
+echo "\nConfiguring JAVA"
+sudo ln -sfn /usr/local/opt/openjdk@8/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-8.jdk
+echo 'export PATH="/usr/local/opt/openjdk@8/bin:$PATH"' >> ~/.zshrc
+yes | sudo ~/Library/Android/sdk/tools/bin/sdkmanager --licenses
+
+
+echo "\nInstalling RVM"
+\curl -sSL https://get.rvm.io | bash -s stable
 
 echo "\nDone!"
